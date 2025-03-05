825bb4c672541d45f7ec7d5ef15d7ef78ab25445
Manuel Galvez <galvezm779@gmail.com>
2025-03-04 18:30:55 -0600

    Commit 1: Agregado array 'amigos' para almacenar los nombres

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// 1. Creación del array para almacenar los nombres de los amigos
+let amigos = [];
